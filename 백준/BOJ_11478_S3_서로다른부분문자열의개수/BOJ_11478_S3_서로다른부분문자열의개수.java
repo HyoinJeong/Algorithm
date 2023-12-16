49caf52c471261b0f8be7ebe6ef11f6c19d8177a
HyoinJeong <hiamy0107@naver.com>
2023-12-16 18:04:02 +0900

    code : BOJ_11478_S3_서로다른부분문자열의개수

--- 백준/BOJ_11478_S3_서로다른부분문자열의개수/BOJ_11478_S3_서로다른부분문자열의개수.java
+++ 백준/BOJ_11478_S3_서로다른부분문자열의개수/BOJ_11478_S3_서로다른부분문자열의개수.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class BOJ_5582_G5_공통부분문자열 {
+public class BOJ_11478_S3_���δٸ��κй��ڿ��ǰ���{
 
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
@@ -10,10 +10,10 @@ public class BOJ_5582_G5_공통부분문자열 {
 		String S = br.readLine();
 		
 		HashSet<String> set = new HashSet<>();
-		for(int i=1; i<=S.length(); i++) { // 부분 문자열 개수
+		for(int i=1; i<=S.length(); i++) { // 부�?문자??개수
 			for(int j=0; j<S.length(); j++) {
-				int index = j+i; // 추출할 부분 문자열의 마지막 인덱스
-				if(index>S.length()) { // 문자열 길이를 초과할 때
+				int index = j+i; // 추출??부�?문자?�의 마�?�??�덱??
+				if(index>S.length()) { // 문자??길이�?초과????
 					break;
 				}
 				String subStr = S.substring(j, index);
